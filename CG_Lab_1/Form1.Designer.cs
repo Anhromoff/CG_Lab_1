e6f270734390bf9510087de1d468ed95b3b6066d
anhromoff <andrey@hromov.com>
2018-04-24 18:51:19 +0300

    Initial commit

---
+++ CG_Lab_1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CG_Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textbox_a = new System.Windows.Forms.MaskedTextBox();
+            this.textbox_step_grid = new System.Windows.Forms.MaskedTextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Build graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "с = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "step grid =";
+            // 
+            // textbox_a
+            // 
+            this.textbox_a.Location = new System.Drawing.Point(54, 3);
+            this.textbox_a.Mask = "000";
+            this.textbox_a.Name = "textbox_a";
+            this.textbox_a.Size = new System.Drawing.Size(85, 20);
+            this.textbox_a.TabIndex = 7;
+            this.textbox_a.Text = "100";
+            // 
+            // textbox_step_grid
+            // 
+            this.textbox_step_grid.Location = new System.Drawing.Point(54, 38);
+            this.textbox_step_grid.Mask = "000";
+            this.textbox_step_grid.Name = "textbox_step_grid";
+            this.textbox_step_grid.Size = new System.Drawing.Size(85, 20);
+            this.textbox_step_grid.TabIndex = 8;
+            this.textbox_step_grid.Text = "50";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 204);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = -5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Visible = false;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.23188F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.76812F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textbox_step_grid, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textbox_a, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 99);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(142, 70);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 79);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox textbox_a;
+        private System.Windows.Forms.MaskedTextBox textbox_step_grid;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CG_Lab_1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CG_Lab_1
+{
+    public partial class Form1 : Form
+    {
+        public Graphics graphics;
+
+        private Pen grid_pen = new Pen(Color.Blue, 0.0001f);
+        private Pen coord_axis_pen = new Pen(Color.Red, 10);
+        private Pen graph_pen = new Pen(Color.Black, 5);
+        private Font font = new Font("Arial", 8);
+        private Brush brush = new SolidBrush(Color.Black);
+        
+        private const double STEP = 0.1;
+        private const int GRAPH_SIZE = (int)((Math.PI) / STEP + 2);
+        private Point[] graph = new Point[GRAPH_SIZE];
+
+        private double step_grid;
+        private double a;
+
+        private int graphics_top;
+        private int graphics_left;
+
+        // Создание формы
+        public Form1()
+        {
+            InitializeComponent();
+            graphics_top = this.Height;
+            graphics_left = this.Width / 3;
+            this.Resize += new EventHandler(this.FormResize);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Обработчик нажатия на кнопку
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            ReadParams();
+            Redraw();
+            trackBar1.Visible = true;
+        }
+
+        // Перерисовка графика
+        private void Redraw()
+        {
+            InitGraphics();
+            graphics.Clear(Color.White);
+
+            // Масштабирование графика в зависимости от состояния ползунка
+            float m = trackBar1.Value;
+            if (m > 0) graphics.ScaleTransform(m, m);
+            else if (m < 0) graphics.ScaleTransform((float)(-1 / m), (float)(-1 / m));
+
+            DrawGrid();
+            graphics.RotateTransform(-90f);
+            DrawGraph();
+        }
+
+        // Считывание введенных данных
+        private void ReadParams()
+        {
+            a = Convert.ToDouble(textbox_a.Text);
+            step_grid = Convert.ToDouble(textbox_step_grid.Text);
+        }
+
+        // Рисование графика
+        private void DrawGraph()
+        {
+            double i = -Math.PI / 2;
+            for (int p=0; p < GRAPH_SIZE; p++)
+            {
+                i += STEP; // делаем шаг
+                double y = X(a, i); // просчитываем координату по y
+                double x = Y(a, i); // просчитываем координату по x
+                graph[p] = new Point(Convert.ToInt32(x), Convert.ToInt32(y)); // заносим в массив точек
+            }
+
+            graphics.DrawCurve(graph_pen, graph); // рисуем
+        }
+
+        // Инициализация объекта graphics
+        private void InitGraphics()
+        {
+            graphics = this.CreateGraphics();
+
+            // Изменение размера области для рисования в соответствии с размерами окна
+            graphics.SetClip(new Rectangle(graphics_left, 0, this.Width - graphics_left, this.Height));
+
+            // Изменение направления осей координат
+            graphics.TranslateTransform((this.Width + graphics_left)/2, this.Height / 2);
+        }
+
+        // Рисование сетки
+        private void DrawGrid()
+        {
+            int left = Convert.ToInt32(graphics.ClipBounds.Left);
+            int right = Convert.ToInt32(graphics.ClipBounds.Right);
+            int bottom = Convert.ToInt32(graphics.ClipBounds.Bottom);
+            int top = Convert.ToInt32(graphics.ClipBounds.Top);
+
+            // Рисуем координатные оси
+            graphics.DrawLine(coord_axis_pen, new Point(left, 0), new Point(right, 0));
+            graphics.DrawLine(coord_axis_pen, new Point(0, bottom), new Point(0, top));
+
+            double x = 0;
+            double y = 0;
+            
+            // Пока не заполним весь прямоугольник
+            while (x < Math.Max(graphics.ClipBounds.Right, graphics.ClipBounds.Bottom))
+            {
+                // Делаем шаг
+                x += step_grid;
+                y += step_grid;
+
+                // Рисуем вертикальные линии (идем вправо и влево от центра)
+                graphics.DrawLine(grid_pen, new Point(Convert.ToInt32(x), bottom), new Point(Convert.ToInt32(x), top));
+                graphics.DrawString(Convert.ToString(x), font, brush, 0, (float)-y);
+                graphics.DrawLine(grid_pen, new Point(Convert.ToInt32(-x), bottom), new Point(Convert.ToInt32(-x), top));
+                graphics.DrawString(Convert.ToString(-x), font, brush, 0, (float)y);
+
+                // Рисуем горизонтальные линии (вверх и вниз от центра)
+                graphics.DrawLine(grid_pen, new Point(left, Convert.ToInt32(y)), new Point(right, Convert.ToInt32(y)));
+                graphics.DrawString(Convert.ToString(y), font, brush, (float)x, 0);
+                graphics.DrawLine(grid_pen, new Point(left, Convert.ToInt32(-y)), new Point(right, Convert.ToInt32(-y)));
+                graphics.DrawString(Convert.ToString(-y), font, brush, (float)-x, 0);
+            }
+        }
+
+        // Высчитываем координату по Х
+        private double X(double a, double t)
+        {
+            double tg = Math.Tan(Math.PI / 4 - t);
+            double p;
+            if (tg >= 0) p = Math.Sqrt(tg);
+            else p = -Math.Sqrt(-tg);
+            return a * Math.Sqrt(2) * ((p + p * p * p) / (1 + p * p * p * p));
+        }
+
+        // Высчитываем координату по Y
+        private double Y(double a, double t)
+        {
+            double tg = Math.Tan(Math.PI / 4 - t);
+            double p;
+            if (tg >= 0) p = Math.Sqrt(tg);
+            else p = -Math.Sqrt(-tg);
+            return a * Math.Sqrt(2) * ((p - p * p * p) / (1 + p * p * p * p));
+        }
+
+        // Обработчик ползунка
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+
+        // Обработчик изменения размера окна
+        private void FormResize(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+    }
+}
